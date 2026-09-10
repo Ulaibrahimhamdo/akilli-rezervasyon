5cc654f6698d1885273eaa1ec7a3863ff68aa68e
Seray Bengisu Onuk <serayybengisuonuk@gmail.com>
2026-09-10 14:44:41 +0300

    Firebase entegrasyonu: environment dosyalari ve firebase.config.ts eklendi

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB2GTU5tiNJmEdfbi5NdmFCRc8dxc6t7Hw",
+    authDomain: "akilli-rezervasyon.firebaseapp.com",
+    projectId: "akilli-rezervasyon",
+    storageBucket: "akilli-rezervasyon.firebasestorage.app",
+    messagingSenderId: "926072772103",
+    appId: "1:926072772103:web:fa34747618739a25f73ca2"
+  }
+};
