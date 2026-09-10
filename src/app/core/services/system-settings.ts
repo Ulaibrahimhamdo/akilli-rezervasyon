fe5bba8ad54ecc168e47728fd25c3badea8ca7e6
Seray Bengisu Onuk <serayybengisuonuk@gmail.com>
2026-09-10 16:06:04 +0300

    SystemSettingsService gercek Firestore okuma mantigiyla yazildi

--- src/app/core/services/system-settings.ts
+++ src/app/core/services/system-settings.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@angular/core';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { SystemSettings as SystemSettingsModel } from '../../models/system-settings.model';
+
+const DEFAULT_SETTINGS: SystemSettingsModel = {
+  monthlyLimit: 3,
+  managerResponseHours: 48,
+  cancelDeadlineHours: 24,
+};
 
 @Injectable({
   providedIn: 'root',
 })
-export class SystemSettings {}
+export class SystemSettings {
+  async getSettings(): Promise<SystemSettingsModel> {
+    const settingsDoc = await getDoc(doc(db, 'systemSettings', 'default'));
+
+    if (!settingsDoc.exists()) {
+      return DEFAULT_SETTINGS;
+    }
+
+    return settingsDoc.data() as SystemSettingsModel;
+  }
+}
